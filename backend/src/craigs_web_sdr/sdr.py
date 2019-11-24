8440e4a642f119e1e175044b8ac4d82f556b65af
Craig J Perry <craigp84@googlemail.com>
2019-11-24 21:47:43 +0000

    feat: Add async SDR sampler component

---
+++ backend/src/craigs_web_sdr/sdr.py
@@ -0,0 +1,101 @@
+"""Manage a singleton RTL SDR dongle and stream samples asynch to a callback.
+
+Prepare an RTL SDR for sample measurements then begin streaming the data.
+Deliver samples data to a caller-provided asynchronous callback. Throttle
+the rate of samples delivered to the callback.
+
+Notes
+-----
+sdr : _AsyncSdrSampler
+    The singleton "sdr" is intended for consumption by clients rather than
+    instantiating an ``_AsyncSdrSampler()`` in each client module. This
+    will avoid contention of the underlying physical RTL SDR device.
+
+    _AsyncSdrSampler only supports 1 RTL SDR at a time and it is not
+    possible to restart streaming after stopping. A new _AsyncSdrSampler()
+    will be required in that case.
+
+    Streaming of samples is done from a (Python, GIL-acquiring) thread in
+    pyrtlsdr. The samples are collected asynchronously via a co-routine in
+    this module.
+
+Examples
+--------
+>>> from craigs_web_sdr.sdr import sdr
+>>> type(sdr)
+<class 'craigs_web_sdr.sdr._AsyncSdrSampler'>
+
+>>> import asyncio
+>>> async def dotter(_):
+...     print('.', end='')
+...
+>>> async def main():
+...     await sdr.start(dotter)
+...
+>>> asyncio.ensure_future(main())
+>>> asyncio.get_event_loop().run_forever()
+.........
+
+"""
+
+
+import logging
+LOGGER = logging.getLogger()
+
+import asyncio
+
+from rtlsdr import RtlSdrAio
+
+
+class _AsyncSdrSampler:
+
+    def __init__(self):
+        self._sdr = None
+        self._callback = None
+
+    def start(self, samples_callback, delay=1):
+        """Begin buffering samples from the SDR
+        
+        Samples are enqueued in a small buffer, when sample consumption
+        doesn't keep pace with samples being enqueued then new samples
+        are dropped. Any delay > 0 guarantees drops, which is often ok.
+
+        Parameters
+        ----------
+        samples_callback : callable
+            callable expecting one parameter - a numpy ndarray of sample data
+        
+        delay : number
+            int or float, throttling delay between callbacks in seconds
+
+        Notes
+        -----
+        New samples are enqueued in real-time in a background thread
+        managed by pyrtlsdr.
+
+        """
+        if not self._sdr:
+            self._callback = samples_callback
+            LOGGER.info("Starting Async SDR Sampler")
+            self._sdr = RtlSdrAio()
+            self._sdr.sample_rate = 1.2e6  # 1.2 MHz, bandwidth
+            self._sdr.center_freq = 102e6  # 102 MHz (in FM broadcast band)
+            self._sdr.gain = 'auto'
+            self._sample_size = self._sdr.sample_rate * 2  # nyquist frequency
+            self._delay = delay
+            asyncio.create_task(self._stream_samples_from_sdr())
+    
+    async def stop(self):
+        """Stop sample collection and relinquish the SDR device"""
+        await self._sdr.stop()
+        self._sdr.close()
+    
+    async def _stream_samples_from_sdr(self):
+        """Throttle the rate of samples sent to the client's callback"""
+        async for samples in self._sdr.stream(num_samples_or_bytes=self._sample_size):
+            LOGGER.debug(f"Got {len(samples)} samples in a {type(samples)}")
+            await self._callback(samples)
+            await asyncio.sleep(self._delay)
+
+
+sdr = _AsyncSdrSampler()
---
+++ backend/tests/test_sdr.py
@@ -0,0 +1,48 @@
+import pytest
+from asynctest import CoroutineMock
+
+import asyncio
+
+from rtlsdr import RtlSdr
+import numpy as np
+
+from craigs_web_sdr.sdr import _AsyncSdrSampler
+
+
+pytestmark = pytest.mark.asyncio  # All test coroutines marked for pytest-asyncio
+
+
+def _has_rtl_sdr():
+    """Convenience for marking tests to run if an RTL SDR is present"""
+    return len(RtlSdr.get_device_serial_addresses()) == 1
+
+
+@pytest.fixture
+async def _async_debug_on(event_loop):
+    """Convenience to enable asyncio debug mode in a test's event loop"""
+    event_loop.set_debug(True)
+
+
+@pytest.fixture
+async def _mocked_callback():
+    """Setup and teardown an _AsyncSdrSampler with a mock callback"""
+    m = CoroutineMock()
+    sdr = _AsyncSdrSampler()
+    sdr.start(m)
+    await asyncio.sleep(5)  # HACK: wait for pyrtlsdr bg thread to init
+    yield m
+    await sdr.stop()
+
+
+@pytest.mark.skipif(not _has_rtl_sdr(), reason="This test requires an RTL SDR dongle")
+async def test_callback_is_provided_with_samples_data(_async_debug_on, _mocked_callback):
+    _mocked_callback.assert_awaited()
+    _mocked_callback.assert_called()
+    
+    latest_callback_param = _mocked_callback.call_args[0][0]
+    assert type(latest_callback_param) is np.ndarray
+    assert len(latest_callback_param) > 0
+
+    sample_data_element = latest_callback_param[0]
+    assert type(sample_data_element) is np.complex128
+    assert sample_data_element != np.complex128()  # i.e. sample is non-zero
